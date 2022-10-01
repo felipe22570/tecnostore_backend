5d56497703ee38ef475216a0b8bd4f6bcbff1989
Felipe <piedra22570@gmail.com>
2022-10-01 18:20:07 -0500

    Add bestbuy api configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import axios from "axios";
+import { categoriesLink, productsLink } from "./urls.js";
 
 const app = express();
 dotenv.config();
@@ -11,4 +13,36 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
 	res.json({ message: "Hello World!" });
+});
+
+app.get("/products/:category/:page?", async (req, res) => {
+	const { category, page } = req.params;
+
+	const link = productsLink(category, page ?? "1");
+	const linkCategory = categoriesLink(category);
+
+	try {
+		const { data } = await axios.get(link);
+		const {
+			data: { name },
+		} = await axios.get(linkCategory);
+
+		res.json({ data, name });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get("/subcategories/:category", async (req, res) => {
+	const { category } = req.params;
+
+	const link = categoriesLink(category);
+
+	try {
+		const { data } = await axios.get(link);
+
+		res.json({ data });
+	} catch (error) {
+		console.log(error);
+	}
 });
