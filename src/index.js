c55024bce88092a7e311d1cb292f73abdab0412d
Felipe <piedra22570@gmail.com>
2022-10-01 18:32:52 -0500

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ app.listen(port, () => {
 	console.log("Running server");
 });
 
+//Enable CORS
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+
 app.get("/", (req, res) => {
 	res.json({ message: "Hello World!" });
 });
